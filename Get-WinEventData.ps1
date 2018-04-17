fa31413bbd96b494d9b8da0467b1d8db0a9f1eea
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-04-17 09:49:27 +0200

    Update Get-WinEventData.ps1

--- Get-WinEventData.ps1
+++ Get-WinEventData.ps1
@@ -1,78 +1,36 @@
-﻿Function Get-WinEventData {
-<#
-.SYNOPSIS
-    Get custom event data from an event log record
+Function Get-WinEventData {
+	<#
+	IN / OUT
+	System.Diagnostics.Eventing.Reader.EventLogRecord
 
-.DESCRIPTION
-    Get custom event data from an event log record
+	.EXAMPLE
+	Get-WinEvent -LogName system -max 1 | Get-WinEventData | Select -Property MachineName, TimeCreated, EventData*
+	Get-WinEvent -ComputerName DomainController1 -FilterHashtable @{Logname='security';id=4740} -MaxEvents 10 | Get-WinEventData | Select TimeCreated, EventDataTargetUserName, EventDataTargetDomainName
+	#>
 
-    Takes in Event Log entries from Get-WinEvent, converts each to XML, extracts all properties from Event.EventData.Data
+	[cmdletbinding()]
+		param (
+			[Parameter(	Mandatory=$true, 
+						ValueFromPipeline=$true,
+						ValueFromPipelineByPropertyName=$true, 
+						ValueFromRemainingArguments=$false, 
+						Position=0 )]
+			[System.Diagnostics.Eventing.Reader.EventLogRecord[]]
+			$event
+		)
 
-    Notes:
-        To avoid overwriting existing properties or skipping event data properties, we append 'EventData' to these extracted properties
-        Some events store custom data in other XML nodes.  For example, AppLocker uses Event.UserData.RuleAndFileData
+	Process {
+		foreach ( $entry in $event ) {
+			$XML = [xml]$entry.ToXml()
+			$XMLData = $null
 
-.PARAMETER Event
-    One or more event.
-    
-    Accepts data from Get-WinEvent or any System.Diagnostics.Eventing.Reader.EventLogRecord object
+			if ( $XMLData = @( $XML.Event.EventData.Data ) ) {
+				for ( $i=0 ; $i -lt $XMLData.count ; $i++ ) {
+					Add-Member -InputObject $entry -MemberType NoteProperty -name "EventData$($XMLData[$i].name)" -Value $XMLData[$i].'#text' -Force
+				}
+			}
 
-.INPUTS
-    System.Diagnostics.Eventing.Reader.EventLogRecord
-
-.OUTPUTS
-    System.Diagnostics.Eventing.Reader.EventLogRecord
-
-.EXAMPLE
-    Get-WinEvent -LogName system -max 1 | Get-WinEventData | Select -Property MachineName, TimeCreated, EventData*
-
-    #  Simple example showing the computer an event was generated on, the time, and any custom event data
-
-.EXAMPLE
-    Get-WinEvent -ComputerName DomainController1 -FilterHashtable @{Logname='security';id=4740} -MaxEvents 10 | Get-WinEventData | Select TimeCreated, EventDataTargetUserName, EventDataTargetDomainName
-
-    #  Find lockout events on a domain controller
-    #    ideally you have log forwarding, audit collection services, or a product from a t-shirt company for this...
-
-.NOTES
-    Concept and most code borrowed from Ashley McGlone
-        http://blogs.technet.com/b/ashleymcglone/archive/2013/08/28/powershell-get-winevent-xml-madness-getting-details-from-event-logs.aspx
-
-.FUNCTIONALITY
-    Computers
-#>
-
-    [cmdletbinding()]
-    param(
-        [Parameter(Mandatory=$true, 
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true, 
-                   ValueFromRemainingArguments=$false, 
-                   Position=0 )]
-        [System.Diagnostics.Eventing.Reader.EventLogRecord[]]
-        $event
-    )
-
-    Process
-    {
-        #Loop through provided events
-        foreach($entry in $event)
-        {
-            #Get the XML...
-            $XML = [xml]$entry.ToXml()
-        
-            #Some events use other nodes, like 'UserData' on Applocker events...
-            $XMLData = $null
-            if( $XMLData = @( $XML.Event.EventData.Data ) )
-            {
-                For( $i=0; $i -lt $XMLData.count; $i++ )
-                {
-                    #We don't want to overwrite properties that might be on the original object, or in another event node.
-                    Add-Member -InputObject $entry -MemberType NoteProperty -name "EventData$($XMLData[$i].name)" -Value $XMLData[$i].'#text' -Force
-                }
-            }
-
-            $entry
-        }
-    }
+			$entry
+		}
+	}
 }
